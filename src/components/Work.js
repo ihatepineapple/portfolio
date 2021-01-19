5eadbae513dbb5fa51036428a799e15426d11a1a
Irene A <ihatepineapple@gmail.com>
2021-01-19 16:20:48 +0100

    work section complete

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import NavBar from "./Navbar";
+import AwesomeImg from "../images/awesome-trivia.png";
+import BirdEimg from "../images/bird-e.png";
+import GoodsImg from "../images/thegoods.png";
 
 function Work() {
     return (
@@ -10,36 +13,55 @@ function Work() {
             <h2>Selected Works</h2>
             <article className="article-wrap">
                 <h3>Awesome Trivia</h3>
-                <p>This is the first project I coded during my Ironhack Web Development Bootcamp.
-                This was coded together with Issa Gruber and Abbie Summers.
-                It's a trivia game using Javascript and Dom manipulation.</p>
-                <Link to="#" className="btn-round">Play Awesome Trivia</Link>
-                <Link to="#" className="btn-round">See the repo on Github</Link>
+                <div className="work-content">
+                    <img className="work-img" src={AwesomeImg} alt="awesome-trivia"/>
+                    <p className="work-info">This is the first project I coded during my Ironhack Web Development Bootcamp. <br/>
+                    This was coded together with Issa Gruber and Abbie Summers.<br/>
+                    It's a trivia game using <b>Javascript</b> and <b>DOM manipulation.</b></p>
+                </div>
+                <div className="btn-wrapper">
+                    <Link to="#" className="btn-round">Play Awesome Trivia</Link>
+                    <Link to="#" className="btn-round">See the repo on Github</Link>
+                </div>
             </article>
 
             <article className="article-wrap">
                 <h3>Bird-e</h3>
-                <p>Bird-e is a collaborative bird watching app that allows users to store all the birds they've seen.
-                It displays the info in a user-friendly way for a better overview. Bird-e has the option of editing and deleting items as well.
-                This was coded together with Sabela Pena.
-                Coded using NodeJS, Express and MongoDB.</p>
-                <Link to="#" className="btn-round">Bird-e Live</Link>
-                <Link to="#" className="btn-round">See the repo on Github</Link>
+                <div className="work-content">
+                    <img className="work-img" src={BirdEimg} alt="bird-e"/>   
+                    <p className="work-info">Bird-e is a collaborative bird watching app that allows users to store all the birds they've seen.
+                    It displays the info in a user-friendly way for a better overview. <br/>
+                    Bird-e has the option of editing and deleting items as well.<br/>
+                    This was coded together with Sabela Pena.
+                    Coded using <b>NodeJS, Express</b> and <b>MongoDB.</b></p>
+                </div>
+                <div className="btn-wrapper">
+                    <Link to="#" className="btn-round">Bird-e Live</Link>
+                    <Link to="#" className="btn-round">See the repo on Github</Link>
+                </div>
             </article>
 
             <article className="article-wrap">
                 <h3>The Goods</h3>
-                <p>This my final project from Ironhack Web Development Bootcamp.
-                The goods is an online networking platform for freelancers of the creative world, where they can showcase their work. 
-                Also business' can search for talent and hire them. Once registered, users can complete their profile and upload their best projects to get hired.
-                Business' can see the projects, see talent profiles and get in touch with them.
-                Coded using ReactJS and MongoDB.</p>
-                <Link to="#" className="btn-round">The Goods Live</Link>
-                <Link to="#" className="btn-round">See the repo on Github</Link>
+                <div className="work-content">
+                    <img className="work-img" src={GoodsImg} alt="the-goods"/>
+                    <p className="work-info">This my final project from Ironhack Web Development Bootcamp. <br/>
+                    The goods is an online networking platform for freelancers of the creative world, where they can showcase their work. <br/>
+                    Users are able to complete their profile and upload their best projects to get hired.
+                    Business' can see the projects, see talent profiles and get in touch with them.<br/>
+                    Coded using <b>ReactJS</b> and <b>MongoDB.</b></p>
+                </div>
+                <div className="btn-wrapper">
+                    <Link to="#" className="btn-round">The Goods Live</Link>
+                    <Link to="#" className="btn-round">See the repo on Github</Link>
+                </div>
             </article>
+            <div className="btn-wrapper-back">
+                <Link to="/" className="menu-btn">Go Back</Link>   
+            </div> 
         </div>
 
-        <Link to="/">Go Back</Link>    
+        
         </div>
     )
 }
